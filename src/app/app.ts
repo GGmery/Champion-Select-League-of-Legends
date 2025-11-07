cfd2c8f2e3e46d91b2c6a38a13362ed96eb72f2f
GGmery <grungemxry@gmail.com>
2025-11-07 11:52:53 +0100

    Actualizado el footer e inicialización del header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer';
+import { HeaderComponent } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
+export class HeaderComponent {
 
 }
