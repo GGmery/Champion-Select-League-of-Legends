ef0b8cd9189d194db92df81bc4939637414d15fc
GGmery <grungemxry@gmail.com>
2025-11-12 11:59:20 +0100

    Estilos provisionales

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer';
 import { HeaderComponent } from './shared/components/header/header';
+import { Sidebar } from "./shared/components/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, Sidebar],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
